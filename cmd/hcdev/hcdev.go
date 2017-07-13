0e08c2e528b511ad51a41da0278dfa256c7a3133
jayDayZee <christopherreay@gmail.com>
2017-07-13 16:54:34 -0700

    using syscall to replace the go preocess with the script. Not sure how to write the test. #204 nailed it :)
    after runing cmd/hcdev/hcdev core branch install, then the installed hcdev was in face the one from this branch

--- cmd/hcdev/hcdev.go
+++ cmd/hcdev/hcdev.go
@@ -6,134 +6,209 @@
 package main
 
 import (
-	_ "errors"
-	_ "fmt"
-	_ "github.com/metacurrency/holochain"
-	_ "github.com/metacurrency/holochain/ui"
-	_ "github.com/urfave/cli"
-	"os"
-	_ "os/user"
-  _    "os/exec"
-	_ "path"
+  // "errors"
+  fmt         "fmt"
+  // holo "github.com/metacurrency/holochain"
+  // "github.com/metacurrency/holochain/ui"
+  cli         "github.com/urfave/cli"
+  os          "os"
+  // "os/user"
+  // exec        "os/exec"
+  syscall     "syscall"
+  // "path"
   filepath    "path/filepath"
-	_ "time"
+  // "time"
 )
 
 const (
-	defaultPort = "4141"
+  defaultPort = "4141"
 )
 
-func LocationOfExecutable() (string) {
-    dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-    return dir
+// ## forget this nonsense, we only need to get "$GOPATH"
+// func LocationOfExecutable() (string) {
+//     dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+//     return dir
+// }
+// func LocationOfBinScripts() (string) {
+//     dir := LocationOfExecutable()
+//     dir = filepath.Join(dir, "../../bin")
+//     return dir
+// }
+func getCurrentDirectory() (string) {
+  dir, err := os.Getwd()
+  if err != nil {
+    fmt.Println("HC: could not find current directory. Weird. Exitting")
+    os.Exit(1)
+  }
+
+  return dir
 }
-func LocationOfBinScripts() (string) {
-    dir := LocationOfExecutable()
-    dir = filepath.Join(dir, "../../bin")
-    return dir
+
+func syscallExec(binaryFile string, args ...string){
+  syscall.Exec(binaryFile, append([]string{binaryFile}, args...), os.Environ())
 }
 
 var debug bool
 var rootPath, devPath, name string
+var coreBranchInstallPath string
+
+func setupApp() (app *cli.App) {
+  app = cli.NewApp()
+  app.Name = "hcdev"
+  app.Usage = "holochain dev command line tool"
+  // app.Version = fmt.Sprintf("0.0.0 (holochain %s)", holo.VersionStr)
+
+  app.Commands = []cli.Command{
+    {
+        Name:      "core",
+        Usage:     fmt.Sprintf("commands relating to Holochain Core"),
+        Subcommands: []cli.Command{
+          {
+            Name:               "branch",
+            // Aliases:    []string{"branch"},
+            Usage:              "install a Holochain Core from a git branch",
+            Flags:              []cli.Flag{
+                cli.StringFlag{
+                 Name:          "sourceDirectory",
+                 Usage:         "path to source files containing checked out git branch, defaults to current directory",
+                 Value:         getCurrentDirectory(),
+                 Destination:   &coreBranchInstallPath,
+                },
+            },
+            Subcommands:    []cli.Command{
+              {
+                Name:           "install",
+                // Aliases:    []string{"branch"},
+                Usage:          "install the version of Holochain Core in '.' onto the host system",
+                Action:         func(c *cli.Context) error {
+                    fmt.Printf  ("HC: core.branch.install: installing from: %v\n",      coreBranchInstallPath)
+
+                    err := os.Chdir(coreBranchInstallPath)
+                    if err != nil {
+                      fmt.Printf("HC: core.branch.install: could not change dir to %v", coreBranchInstallPath)
+                      os.Exit(1)
+                    }
+
+                    // terminates go process
+                    syscallExec(
+                        filepath.Join(
+                          os.Getenv("GOPATH"), 
+                          "src/github.com/metacurrency/holochain", 
+                          "bin", 
+                          "holochain.core.testing.branch",
+                        ),
+                    )
+
+                    return nil
+                },
+              },
+            },
+          },
+        },
+    },
+  }
+
+  return app
+}
 
 // func setupApp() (app *cli.App) {
-// 	app = cli.NewApp()
-// 	app.Name = "hcdev"
-// 	app.Usage = "holochain dev command line tool"
-// 	app.Version = fmt.Sprintf("0.0.0 (holochain %s)", holo.VersionStr)
+//  app = cli.NewApp()
+//  app.Name = "hcdev"
+//  app.Usage = "holochain dev command line tool"
+//  app.Version = fmt.Sprintf("0.0.0 (holochain %s)", holo.VersionStr)
 
-// 	var service *holo.Service
+//  var service *holo.Service
 
-// 	app.Flags = []cli.Flag{
-// 		cli.BoolFlag{
-// 			Name:        "debug",
-// 			Usage:       "debugging output",
-// 			Destination: &debug,
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "execpath",
-// 			Usage:       "path to holochain dev execution directory (default: ~/.holochaindev)",
-// 			Destination: &rootPath,
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "path",
-// 			Usage:       "path to chain source definition directory (default: current working dir)",
-// 			Destination: &devPath,
-// 		},
-// 	}
-// 	app.Commands = []cli.Command{
-// 		{
-// 			Name:      "test",
-// 			Aliases:   []string{"t"},
-// 			ArgsUsage: "no args run's all stand-alone | [test file prefix] | [scenario] [role]",
-// 			Usage:     "run chain's stand-alone or scenario tests",
-// 			Action: func(c *cli.Context) error {
-// 				var err error
-// 				var h *holo.Holochain
-// 				h, err = getHolochain(c, service)
+//  app.Flags = []cli.Flag{
+//    cli.BoolFlag{
+//      Name:        "debug",
+//      Usage:       "debugging output",
+//      Destination: &debug,
+//    },
+//    cli.StringFlag{
+//      Name:        "execpath",
+//      Usage:       "path to holochain dev execution directory (default: ~/.holochaindev)",
+//      Destination: &rootPath,
+//    },
+//    cli.StringFlag{
+//      Name:        "path",
+//      Usage:       "path to chain source definition directory (default: current working dir)",
+//      Destination: &devPath,
+//    },
+//  }
+//  app.Commands = []cli.Command{
+//    {
+//      Name:      "test",
+//      Aliases:   []string{"t"},
+//      ArgsUsage: "no args run's all stand-alone | [test file prefix] | [scenario] [role]",
+//      Usage:     "run chain's stand-alone or scenario tests",
+//      Action: func(c *cli.Context) error {
+//        var err error
+//        var h *holo.Holochain
+//        h, err = getHolochain(c, service)
 
-// 				args := c.Args()
-// 				var errs []error
+//        args := c.Args()
+//        var errs []error
 
-// 				if len(args) == 2 {
-// 					dir := h.TestPath() + "/" + args[0]
-// 					role := args[1]
+//        if len(args) == 2 {
+//          dir := h.TestPath() + "/" + args[0]
+//          role := args[1]
 
-// 					err, errs = h.TestScenario(dir, role)
-// 					if err != nil {
-// 						return err
-// 					}
-// 				} else if len(args) == 1 {
-// 					errs = h.TestOne(args[0])
-// 				} else if len(args) == 0 {
-// 					errs = h.Test()
-// 				} else {
-// 					return errors.New("test: expected 0 args (run all stand-alone tests), 1 arg (a single stand-alone test) or 2 args (scenario and role)")
-// 				}
+//          err, errs = h.TestScenario(dir, role)
+//          if err != nil {
+//            return err
+//          }
+//        } else if len(args) == 1 {
+//          errs = h.TestOne(args[0])
+//        } else if len(args) == 0 {
+//          errs = h.Test()
+//        } else {
+//          return errors.New("test: expected 0 args (run all stand-alone tests), 1 arg (a single stand-alone test) or 2 args (scenario and role)")
+//        }
 
-// 				var s string
-// 				for _, e := range errs {
-// 					s += e.Error()
-// 				}
-// 				if s != "" {
-// 					return errors.New(s)
-// 				}
-// 				return nil
-// 			},
-// 		},
-// 		{
-// 			Name:      "web",
-// 			Aliases:   []string{"serve", "w"},
-// 			ArgsUsage: "[port]",
-// 			Usage:     fmt.Sprintf("serve a chain to the web on localhost:<port> (defaults to %s)", defaultPort),
-// 			Action: func(c *cli.Context) error {
-// 				h, err := getHolochain(c, service)
-// 				if err != nil {
-// 					return err
-// 				}
-// 				h, err = service.GenChain(name)
-// 				if err != nil {
-// 					return err
-// 				}
+//        var s string
+//        for _, e := range errs {
+//          s += e.Error()
+//        }
+//        if s != "" {
+//          return errors.New(s)
+//        }
+//        return nil
+//      },
+//    },
+//    {
+//      Name:      "web",
+//      Aliases:   []string{"serve", "w"},
+//      ArgsUsage: "[port]",
+//      Usage:     fmt.Sprintf("serve a chain to the web on localhost:<port> (defaults to %s)", defaultPort),
+//      Action: func(c *cli.Context) error {
+//        h, err := getHolochain(c, service)
+//        if err != nil {
+//          return err
+//        }
+//        h, err = service.GenChain(name)
+//        if err != nil {
+//          return err
+//        }
 
-// 				var port string
-// 				if len(c.Args()) == 0 {
-// 					port = defaultPort
-// 				} else {
-// 					port = c.Args()[0]
-// 				}
-// 				fmt.Printf("Serving holochain with DNA hash:%v on port:%s\n", h.DNAHash(), port)
+//        var port string
+//        if len(c.Args()) == 0 {
+//          port = defaultPort
+//        } else {
+//          port = c.Args()[0]
+//        }
+//        fmt.Printf("Serving holochain with DNA hash:%v on port:%s\n", h.DNAHash(), port)
 
-// 				err = h.Activate()
-// 				if err != nil {
-// 					return err
-// 				}
-// 				//				go h.DHT().HandleChangeReqs()
-// 				go h.DHT().HandleGossipWiths()
-// 				go h.DHT().Gossip(2 * time.Second)
-// 				ui.NewWebServer(h, port).Start()
-// 				return err
-// 			},
+//        err = h.Activate()
+//        if err != nil {
+//          return err
+//        }
+//        //        go h.DHT().HandleChangeReqs()
+//        go h.DHT().HandleGossipWiths()
+//        go h.DHT().Gossip(2 * time.Second)
+//        ui.NewWebServer(h, port).Start()
+//        return err
+//      },
 
 //       {
 //       Name:      "core",
@@ -159,84 +234,84 @@ var rootPath, devPath, name string
 //           },
 //         },
 //       },
-// 		}}
+//    }}
 
-// 	app.Before = func(c *cli.Context) error {
-// 		if debug {
-// 			os.Setenv("DEBUG", "1")
-// 		}
-// 		holo.Initialize()
+//  app.Before = func(c *cli.Context) error {
+//    if debug {
+//      os.Setenv("DEBUG", "1")
+//    }
+//    holo.Initialize()
 
-// 		var err error
-// 		if devPath == "" {
-// 			devPath, err = os.Getwd()
-// 			if err != nil {
-// 				return err
-// 			}
-// 		}
-// 		name = path.Base(devPath)
-// 		// TODO confirm devPath is actually a holochain app directory
+//    var err error
+//    if devPath == "" {
+//      devPath, err = os.Getwd()
+//      if err != nil {
+//        return err
+//      }
+//    }
+//    name = path.Base(devPath)
+//    // TODO confirm devPath is actually a holochain app directory
 
-// 		if rootPath == "" {
-// 			rootPath = os.Getenv("HOLOPATH")
-// 			if rootPath == "" {
-// 				u, err := user.Current()
-// 				if err != nil {
-// 					return err
-// 				}
-// 				userPath := u.HomeDir
-// 				rootPath = userPath + "/" + holo.DefaultDirectoryName + "dev"
-// 			}
-// 		}
-// 		if !holo.IsInitialized(rootPath) {
-// 			service, err = holo.Init(rootPath, holo.AgentName("test@example.com"))
-// 			if err != nil {
-// 				return err
-// 			}
-// 			fmt.Println("Holochain dev service initialized:")
-// 			fmt.Printf("    %s directory created\n", rootPath)
-// 			fmt.Printf("    defaults stored to %s\n", holo.SysFileName)
-// 			fmt.Println("    key-pair generated")
-// 			fmt.Printf("    default agent stored to %s\n", holo.AgentFileName)
+//    if rootPath == "" {
+//      rootPath = os.Getenv("HOLOPATH")
+//      if rootPath == "" {
+//        u, err := user.Current()
+//        if err != nil {
+//          return err
+//        }
+//        userPath := u.HomeDir
+//        rootPath = userPath + "/" + holo.DefaultDirectoryName + "dev"
+//      }
+//    }
+//    if !holo.IsInitialized(rootPath) {
+//      service, err = holo.Init(rootPath, holo.AgentName("test@example.com"))
+//      if err != nil {
+//        return err
+//      }
+//      fmt.Println("Holochain dev service initialized:")
+//      fmt.Printf("    %s directory created\n", rootPath)
+//      fmt.Printf("    defaults stored to %s\n", holo.SysFileName)
+//      fmt.Println("    key-pair generated")
+//      fmt.Printf("    default agent stored to %s\n", holo.AgentFileName)
 
-// 		} else {
-// 			service, err = holo.LoadService(rootPath)
-// 		}
-// 		return err
-// 	}
+//    } else {
+//      service, err = holo.LoadService(rootPath)
+//    }
+//    return err
+//  }
 
-// 	app.Action = func(c *cli.Context) error {
-// 		cli.ShowAppHelp(c)
+//  app.Action = func(c *cli.Context) error {
+//    cli.ShowAppHelp(c)
 
-// 		return nil
-// 	}
-// 	return
+//    return nil
+//  }
+//  return
 
 // }
 
 func main() {
-	// app := setupApp()
+  app := setupApp()
 
-	// err := app.Run(os.Args)
-	// if err != nil {
-	// 	fmt.Printf("Error: %v\n", err)
-	// 	os.Exit(1)
-	// }
+  err := app.Run(os.Args)
+  if err != nil {
+   fmt.Printf("Error: %v\n", err)
+   os.Exit(1)
+  }
 }
 
 // func getHolochain(c *cli.Context, service *holo.Service) (h *holo.Holochain, err error) {
-// 	fmt.Printf("Copying chain to: %s\n", rootPath)
-// 	err = os.RemoveAll(rootPath + "/" + name)
-// 	if err != nil {
-// 		return
-// 	}
-// 	h, err = service.Clone(devPath, rootPath+"/"+name, false)
-// 	if err != nil {
-// 		return
-// 	}
-// 	h, err = service.Load(name)
-// 	if err != nil {
-// 		return
-// 	}
-// 	return
+//  fmt.Printf("Copying chain to: %s\n", rootPath)
+//  err = os.RemoveAll(rootPath + "/" + name)
+//  if err != nil {
+//    return
+//  }
+//  h, err = service.Clone(devPath, rootPath+"/"+name, false)
+//  if err != nil {
+//    return
+//  }
+//  h, err = service.Load(name)
+//  if err != nil {
+//    return
+//  }
+//  return
 // }
--- cmd/hcdev/hcdev_test.go
+++ cmd/hcdev/hcdev_test.go
@@ -2,34 +2,45 @@ package main
 
 import (
   fmt         "fmt"
-  strings     "strings"
+  // strings     "strings"
 
   .           "github.com/smartystreets/goconvey/convey"
   _           "github.com/urfave/cli"
   testing     "testing"
 )
 
-// func TestSetupApp(t *testing.T) {
-//  app := setupApp()
-//  Convey("it should create the cli App", t, func() {
-//    So(app.Name, ShouldEqual, "hcdev")
-//  })
-// }
-
-func TestLocationOfExecutable(t *testing.T) {
-  Convey("should print the location of the executable", t, func() {
-    location  := LocationOfExecutable()
-    fmt.Sprintf("locationOfExecutable: %v", location)
-    fmt.Println(location)
-    So( strings.Contains(location, "/cmd/hcdev/") , ShouldBeTrue)
-  })
-
-  // ##output
-  // /tmp/go-build500264145/_/home/christopher/dev.bin2hcdcev/cmd/hcdev/_test
-  // .
-  // 1 total assertion
-
-  // PASS
-  // ok      _/home/christopher/dev.bin2hcdcev/cmd/hcdev     0.003s
-
+func TestSetupApp(t *testing.T) {
+ app := setupApp()
+ Convey("it should create the cli App", t, func() {
+   fmt.Printf("app.Name: %v", app.Name)
+   So(app.Name, ShouldEqual, "hcdev")
+ })
 }
+
+func TestRunLocalSourceInstall(t *testing.T) {
+ os.Args = []string{"core", "branch", "install"} 
+
+ app := setupApp()
+ Convey("it should create the cli App", t, func() {
+   fmt.Printf("app.Name: %v", app.Name)
+   So(app.Name, ShouldEqual, "hcdev")
+ })
+}
+
+// func TestLocationOfExecutable(t *testing.T) {
+//   Convey("should print the location of the executable", t, func() {
+//     location  := LocationOfExecutable()
+//     fmt.Sprintf("locationOfExecutable: %v", location)
+//     fmt.Println(location)
+//     So( strings.Contains(location, "/cmd/hcdev/") , ShouldBeTrue)
+//   })
+
+//   // ##output
+//   // /tmp/go-build500264145/_/home/christopher/dev.bin2hcdcev/cmd/hcdev/_test
+//   // .
+//   // 1 total assertion
+
+//   // PASS
+//   // ok      _/home/christopher/dev.bin2hcdcev/cmd/hcdev     0.003s
+
+// }
