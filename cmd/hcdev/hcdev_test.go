bcb2a401ab96682ca94d92d2001a779e1ea4fe29
jayDayZee <christopherreay@gmail.com>
2017-07-13 15:01:35 -0700

    made test success and added Println output, which prints, where Sprintf stlil doesnt #204

--- cmd/hcdev/hcdev_test.go
+++ cmd/hcdev/hcdev_test.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-  "fmt"
+  fmt         "fmt"
+  strings     "strings"
 
-	.          "github.com/smartystreets/goconvey/convey"
-	_          "github.com/urfave/cli"
-	testing    "testing"
+  .           "github.com/smartystreets/goconvey/convey"
+  _           "github.com/urfave/cli"
+  testing     "testing"
 )
 
 // func TestSetupApp(t *testing.T) {
-// 	app := setupApp()
-// 	Convey("it should create the cli App", t, func() {
-// 		So(app.Name, ShouldEqual, "hcdev")
-// 	})
+//  app := setupApp()
+//  Convey("it should create the cli App", t, func() {
+//    So(app.Name, ShouldEqual, "hcdev")
+//  })
 // }
 
 func TestLocationOfExecutable(t *testing.T) {
   Convey("should print the location of the executable", t, func() {
     location  := LocationOfExecutable()
     fmt.Sprintf("locationOfExecutable: %v", location)
-      So(location, ShouldEqual, "something")
+    fmt.Println(location)
+    So( strings.Contains(location, "/cmd/hcdev/") , ShouldBeTrue)
   })
+
+  // ##output
+  // /tmp/go-build500264145/_/home/christopher/dev.bin2hcdcev/cmd/hcdev/_test
+  // .
+  // 1 total assertion
+
+  // PASS
+  // ok      _/home/christopher/dev.bin2hcdcev/cmd/hcdev     0.003s
+
 }
